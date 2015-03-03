dd6889d49e2f06305ec77fa289a0b8c26cc742a6
gioadami <giordano.adami@gmail.com>
2015-03-03 11:42:31 +0100

    Security.

---
+++ sco.riciclo-web/src/main/java/it/smartcommunitylab/riciclo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package it.smartcommunitylab.riciclo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationProvider customAuthenticationProvider;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(customAuthenticationProvider);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/","/giudicarie/**").authenticated().anyRequest().permitAll();
+		http.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+	}
+}
