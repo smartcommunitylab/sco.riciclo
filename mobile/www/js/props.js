d8510a2f5bb6db55e8ae862cdc95d845d8a4ec20
kazhamiakin <kazhamiakin@gmail.com>
2015-06-03 23:45:42 +0200

    resources updated

--- mobile/www/js/props.js
+++ mobile/www/js/props.js
@@ -15,14 +15,14 @@
 
 // TRENTO
 APP_ID = "TRENTO";
-DATA_VERSION = 10;
+DATA_VERSION = 12;
 API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
 //SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
 VERSION = "1.0";
 APP_NAME="100% Riciclo - Trento";
 ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple'};
 ICON_POINT_MAP = {'Isola ecologica': 'isola_eco', 'CRM': 'crm', 'CRZ': 'crz', 'Farmacia': 'farmacia', 'Rivenditore': 'rivenditore', 'Porta a porta carta':'porta_a_porta', 'Porta a porta organico':'porta_a_porta', 'Porta a porta residuo':'porta_a_porta', 'Porta a porta cartone':'porta_a_porta', 'Porta a porta plastica e metallo':'porta_a_porta', 'Porta a porta carta e cartone':'porta_a_porta', 'Porta a porta imballaggi leggeri':'porta_a_porta', 'Porta a porta vetro':'porta_a_porta', 'Porta a porta sfalci e potatura':'porta_a_porta', 'Ditta specializzata':'furgone', 'Esercenti convenzionati':'farmacia'};
-SHOW_NEWS = true;
+SHOW_NEWS = false;
 FEED_URL = "http://www.asvo.it/feed/";
 EXT_URL = "http://www.asvo.it";
 IF_HIDDEN_FIELDS = false;
