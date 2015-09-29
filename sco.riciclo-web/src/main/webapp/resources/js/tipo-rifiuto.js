c79b3f47b200df1ab2cac0e5a3452c6ab7d4091b
micnori <micnori@yahoo.com>
2015-09-29 15:49:10 +0200

    add tipologia rifiuto

---
+++ sco.riciclo-web/src/main/webapp/resources/js/tipo-rifiuto.js
@@ -0,0 +1,222 @@
+angular.module('tipo-rifiuto', ['DataService']).controller('userCtrl', function($scope, $http, DataService) {
+	DataService.getProfile().then(function(p) {
+  	$scope.initData(p);
+  });
+
+	$scope.selectedTab = "menu-tipo-rifiuto";
+	$scope.language = "it";
+	$scope.draft = true;
+	$scope.defaultLang = "it";
+	
+	$scope.fId = "";
+	$scope.fNome = "";
+	$scope.fDescrizione = "";
+	$scope.search = "";
+	$scope.actualName = "";
+	
+	$scope.edit = false;
+	$scope.create = false;
+	$scope.incomplete = true;
+	
+	$scope.error = false;
+	$scope.errorMsg = "";
+	
+	$scope.ok = false;
+	$scope.okMsg = "";
+	
+	$scope.data = null;
+	$scope.status = 200;
+	
+	$scope.tipoRifiutoList = [];
+	
+	$scope.initData = function(profile) {
+		$scope.profile = profile;
+		
+		var urlTipologiaUtenza = "api/tipologia/rifiuto/" + $scope.profile.appInfo.ownerId + "?draft=" + $scope.draft;
+		$http.get(urlTipologiaUtenza, {headers: {'X-ACCESS-TOKEN': $scope.profile.appInfo.token}}).success(function (response) {
+			$scope.tipoRifiutoList = response;
+		});
+	};
+	
+	$scope.resetError = function() {
+		$scope.error = false;
+		$scope.errorMsg = "";
+	};
+	
+	$scope.resetOk = function() {
+		$scope.ok = false;
+		$scope.okMsg = "";
+	};
+	
+	$scope.changeLanguage = function(language) {
+		$scope.language = language;
+		if($scope.edit && ($scope.fId != null)) {
+			var element = $scope.findByObjectId($scope.tipoRifiutoList, $scope.fId);
+			if(element != null) {
+				$scope.fNome = element.nome[$scope.language];
+				$scope.fDescrizione = element.descrizione[$scope.language];
+			}
+		}
+	};
+	
+	$scope.editTipo = function(id) {
+		console.log("editTipo:" + id);
+		//var index = $scope.findIndex($scope.rifiuti, id);
+		//console.log("editRifiutoPos:" + index);
+		$scope.edit = true;
+		$scope.create = false;
+		var element = $scope.findByObjectId($scope.tipoRifiutoList, id);
+		if(element != null) {
+			$scope.fId = id;
+			$scope.fNome = element.nome[$scope.language];
+			$scope.fDescrizione = element.descrizione[$scope.language];
+			$scope.actualName = element.nome[$scope.defaultLang];
+			$scope.incomplete = false;	
+		}
+		$('html,body').animate({scrollTop:0},0);
+	};
+	
+	$scope.newTipo = function() {
+		console.log("newTipo");
+		$scope.edit = false;
+		$scope.create = true;
+		$scope.fId = "";
+		$scope.fNome = "";
+		$scope.fDescrizione = "";
+		$scope.actualName = "";
+		$scope.incomplete = true;
+	};
+	
+	$scope.resetUI = function() {
+		$scope.edit = false;
+		$scope.create = false;
+		$scope.fId = "";
+		$scope.fNome = "";
+		$scope.fDescrizione = "";
+		$scope.search = "";
+		$scope.actualName = "";
+		$scope.incomplete = true;
+		$('html,body').animate({scrollTop:0},0);
+	};
+	
+	$scope.saveTipo = function() {
+		if($scope.create) {
+			var element = {
+				objectId: '',
+				nome: {},
+				descrizione: {}
+			};
+			element.objectId = $scope.fId;
+			element.nome[$scope.language] = $scope.fNome;
+			element.descrizione[$scope.language] = $scope.fDescrizione;
+			var copiedList = $scope.tipoRifiutoList.slice(0);
+			copiedList.unshift(element);
+			var url = "api/tipologia/rifiuto/" + $scope.profile.appInfo.ownerId + "?draft=" + $scope.draft; 
+			$http.post(url, copiedList, {headers: {'X-ACCESS-TOKEN': $scope.profile.appInfo.token}}).then(
+			function(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+		  	$scope.status = response.status;
+		  	$scope.data = response.data;
+		  	$scope.ok = true;
+		  	$scope.okMsg = "Operazione eseguita con successo";
+		  	$scope.tipoRifiutoList = copiedList;
+		  	$scope.resetUI();
+		  	console.log("saveTipo:" + response.status + " - " + response.data);
+		  }, 
+		  function(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  	$scope.error = true;
+		  	$scope.errorMsg = response.status + " - " + (response.data || "Request failed");
+		  	$scope.status = response.status;
+		  });
+		}
+		if($scope.edit) {
+			var index = $scope.findIndex($scope.tipoRifiutoList, $scope.fId);
+			if(index >= 0) {
+				var element = $scope.tipoRifiutoList[index];
+				if(element != null) {
+					element.nome[$scope.language] = $scope.fNome;
+					element.descrizione[$scope.language] = $scope.fDescrizione;
+					var url = "api/tipologia/rifiuto/" + $scope.profile.appInfo.ownerId + "?draft=" + $scope.draft;
+					$http.post(url, $scope.tipoRifiutoList, {headers: {'X-ACCESS-TOKEN': $scope.profile.appInfo.token}}).then(
+					function(response) {
+				    // this callback will be called asynchronously
+				    // when the response is available
+				  	$scope.status = response.status;
+				  	$scope.data = response.data;
+				  	$scope.ok = true;
+				  	$scope.okMsg = "Operazione eseguita con successo";
+				  	console.log("saveTipo:" + response.status + " - " + response.data);
+				  }, 
+				  function(response) {
+				    // called asynchronously if an error occurs
+				    // or server returns response with an error status.
+				  	$scope.error = true;
+				  	$scope.errorMsg = response.status + " - " + (response.data || "Request failed");
+				  	$scope.status = response.status;
+				  });					
+				}
+			}
+		}
+	};
+	
+	$scope.deleteTipo = function(id) {
+		var index = $scope.findIndex($scope.tipoRifiutoList, id);
+		if(index >= 0) {
+			var copiedList = $scope.tipoRifiutoList.slice(0);
+			copiedList.splice(index, 1);
+			var url = "api/tipologia/rifiuto/" + $scope.profile.appInfo.ownerId + "?draft=" + $scope.draft;
+			$http.post(url, copiedList, {headers: {'X-ACCESS-TOKEN': $scope.profile.appInfo.token}}).then(
+			function(response) {
+				// this callback will be called asynchronously
+				// when the response is available
+				$scope.status = response.status;
+				$scope.data = response.data;
+				$scope.ok = true;
+				$scope.okMsg = "Operazione eseguita con successo";
+				$scope.tipoRifiutoList = copiedList;
+				$scope.resetUI();
+				console.log("deleteTipo:" + response.status + " - " + response.data);
+			}, 
+			function(response) {
+			  // called asynchronously if an error occurs
+				// or server returns response with an error status.
+				$scope.error = true;
+				$scope.errorMsg = response.status + " - " + (response.data || "Request failed");
+				$scope.status = response.status;
+			});			
+		}
+	};
+	
+	$scope.$watch('fNome',function() {$scope.test();});
+	$scope.$watch('fId',function() {$scope.test();});
+	
+	$scope.findByObjectId = function(array, id) {
+    for (var d = 0, len = array.length; d < len; d += 1) {
+      if (array[d].objectId === id) {
+          return array[d];
+      }
+    }
+    return null;
+	};
+	
+	$scope.findIndex = function(array, id) {
+		for (var d = 0, len = array.length; d < len; d += 1) {
+			if (array[d].objectId === id) {
+				return d;
+			}
+		}
+		return -1;
+	};
+	
+	$scope.test = function() {
+		if (($scope.fNome == null) || ($scope.fNome.length <= 3) ||
+				($scope.fId == null) || ($scope.fId.length <= 3)) {
+	    $scope.incomplete = true;
+	  } else {
+	  	$scope.incomplete = false;
+	  }
+	};	
+});
