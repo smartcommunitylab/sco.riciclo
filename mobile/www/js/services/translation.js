ae1282f96ae56506d2c1fcbf7cca023a0bb85049
emmartu <lelemartufi@gmail.com>
2015-11-16 11:47:09 +0100

    reload data button for dev mode

--- mobile/www/js/services/translation.js
+++ mobile/www/js/services/translation.js
@@ -1,4 +1,5 @@
 ITALIAN_VERSION = {
+        reload_data: "Ricarica i Dati",
         porta_a_porta: "Porta a Porta",
         connection_error: "La connessione con il server non è disponibile",
         tomorrow_at: "Domani a ",
@@ -128,6 +129,7 @@ ITALIAN_VERSION = {
 };
 
 ENGLISH_VERSION = {
+        reload_data: "Reload Data",
         porta_a_porta: "Curbside Collection",
         connection_error: "The connection to the server is unavailable",
         tomorrow_at: "Tomorrow at ",
