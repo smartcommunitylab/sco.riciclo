f1bda7a8691e572c18d7f2e062538e2e19cc51ca
micnori <micnori@yahoo.com>
2015-11-17 16:32:49 +0100

    fix retrieve gestore

--- sco.riciclo-web/src/main/java/it/smartcommunitylab/riciclo/controller/Utils.java
+++ sco.riciclo-web/src/main/java/it/smartcommunitylab/riciclo/controller/Utils.java
@@ -113,13 +113,13 @@ public class Utils {
 	public static void findGestori(Map<String, Area> mapArea, String ownerId, boolean draft,
 			Map<String, Gestore> resultMap, RepositoryManager storage) throws ClassNotFoundException {
 		for(Area area : mapArea.values()) {
-			String gestoreId = area.getGestore();
+			String ragioneSociale = area.getGestore();
 			//find Gestore rows for specific area
-			Criteria criteriaId = new Criteria("objectId").is(gestoreId);
-			Gestore gestore = (Gestore) storage.findOneData(Gestore.class, criteriaId, ownerId, draft) ;
+			Criteria criteriaRS = new Criteria("ragioneSociale").is(ragioneSociale);
+			Gestore gestore = (Gestore) storage.findOneData(Gestore.class, criteriaRS, ownerId, draft) ;
 			//add data to result map
 			if(gestore != null) {
-				resultMap.put(gestoreId, gestore);
+				resultMap.put(gestore.getObjectId(), gestore);
 			}
 		}
 	}
