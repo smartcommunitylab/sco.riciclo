fe08338d6b17a7a5c263d76f66bb753da1749230
emmartu <lelemartufi@gmail.com>
2015-12-15 12:12:21 +0100

    rotaliana features

--- mobile/www/js/props.js
+++ mobile/www/js/props.js
@@ -1,11 +1,11 @@
-// TRENTO
 APP_ID = "ASIA-ROT";
-DATA_VERSION = 11;
+DATA_VERSION = 1;
 API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
 //SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
-VERSION = "1.0.3";
-APP_NAME="100% Riciclo - Asia";
-APP_NAME_CREDITS = "100% Riciclo";
+VERSION = "1.0.0";
+APP_NAME="100% Riciclo - Rotaliana";
+APP_NAME_CREDITS = "100% Riciclo<br/>Rotaliana";
+APP_LOCATION_CREDITS = "Comunità Rotaliana";
 ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
 ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 'CRM': 'riciclo-crm', 'CRZ': 'riciclo-crz', 'Farmacia': 'riciclo-farmacia', 'Rivenditore': 'riciclo-rivenditore', 'Porta a porta carta':'riciclo-porta_a_porta', 'Porta a porta organico':'riciclo-porta_a_porta', 'Porta a porta residuo':'riciclo-porta_a_porta', 'Porta a porta cartone':'riciclo-porta_a_porta', 'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 'Porta a porta carta e cartone':'riciclo-porta_a_porta', 'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 'Porta a porta vetro':'riciclo-porta_a_porta', 'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 'Ditta specializzata':'riciclo-furgone', 'Esercenti convenzionati':'riciclo-farmacia', 'tipoPuntoRaccoltaUno':'riciclo-farmacia'};
 SHOW_NEWS = true;
@@ -18,14 +18,13 @@ ENDPOINT_URL = 'http://localhost:8001/riciclo';
 //ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
 PORTA_A_PORTA_LABEL = 'porta_a_porta';
 
-USE_DRAFT = true;
+USE_DRAFT = false;
 LANG = ['it'];
-//COMUNI_LIST = ['022205','022003','022034'];
 COMUNI_LIST = ['022080','022126','022160','022167','022225','022116','022117','022103'];
 
 
-ZOOM = 11;
+ZOOM = 11,5;
 CENTER = {
-      latitude: 46.2056269,
-      longitude: 11.1096869
+      latitude: 46.1555397,
+      longitude: 11.1601932
 };
