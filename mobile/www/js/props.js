486da6ad082026ff916b669867da4039336a0f3c
emmartu <lelemartufi@gmail.com>
2016-01-14 13:53:06 +0100

    config folder updated with immagini map file

--- mobile/www/js/props.js
+++ mobile/www/js/props.js
@@ -2,26 +2,23 @@ APP_ID = "ASIA-ROT";
 DATA_VERSION = 1;
 API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
 //SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
-VERSION = "1.0.0";
+VERSION = "1.0.2";
 APP_NAME="100% Riciclo - Rotaliana";
 APP_NAME_CREDITS = "100% Riciclo<br/>Rotaliana";
 APP_LOCATION_CREDITS = "Comunità Rotaliana";
-ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
-ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 'CRM': 'riciclo-crm', 'CRZ': 'riciclo-crz', 'Farmacia': 'riciclo-farmacia', 'Rivenditore': 'riciclo-rivenditore', 'Porta a porta carta':'riciclo-porta_a_porta', 'Porta a porta organico':'riciclo-porta_a_porta', 'Porta a porta residuo':'riciclo-porta_a_porta', 'Porta a porta cartone':'riciclo-porta_a_porta', 'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 'Porta a porta carta e cartone':'riciclo-porta_a_porta', 'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 'Porta a porta vetro':'riciclo-porta_a_porta', 'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 'Ditta specializzata':'riciclo-furgone', 'Esercenti convenzionati':'riciclo-farmacia', 'tipoPuntoRaccoltaUno':'riciclo-farmacia'};
+ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple'};
+ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 'CRM': 'riciclo-crm', 'CRZ': 'riciclo-crz', 'Farmacia': 'riciclo-farmacia', 'Rivenditore': 'riciclo-rivenditore', 'Porta a porta carta':'riciclo-porta_a_porta', 'Porta a porta organico':'riciclo-porta_a_porta', 'Porta a porta residuo':'riciclo-porta_a_porta', 'Porta a porta cartone':'riciclo-porta_a_porta', 'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 'Porta a porta carta e cartone':'riciclo-porta_a_porta', 'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 'Porta a porta vetro':'riciclo-porta_a_porta', 'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 'Ditta specializzata':'riciclo-furgone', 'Esercenti convenzionati':'riciclo-farmacia'};
 SHOW_NEWS = true;
-FEED_URL = "http://www.asvo.it/feed/";
-EXT_URL = "http://www.asvo.it";
+FEED_URL = "http://www.asia.tn.it/rss/feed/avvisi/";
+EXT_URL = "http://www.asia.tn.it/";
 IF_HIDDEN_FIELDS = false;
 CUSTOM_PAP = [];
-//ENDPOINT_URL = 'https://tn.smartcommunitylab.it/riciclo';
-ENDPOINT_URL = 'http://localhost:8001/riciclo';
-//ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
+ENDPOINT_URL = 'https://tn.smartcommunitylab.it/riciclo2';
 PORTA_A_PORTA_LABEL = 'porta_a_porta';
 
 USE_DRAFT = false;
 LANG = ['it'];
-COMUNI_LIST = ['022080','022126','022160','022167','022225','022116','022117','022103'];
-
+COMUNI_LIST = [];
 
 ZOOM = 11,5;
 CENTER = {
