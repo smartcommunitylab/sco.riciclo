ed22430c78350239a0b3f6feb9b5e5805cfc628e
micnori <micnori@yahoo.com>
2016-01-22 13:50:18 +0100

    aggiunto config per ASIA-ADIGE

---
+++ mobile/config/instances/ASIA-ADIGE/www/js/props.js
@@ -0,0 +1,27 @@
+APP_ID = "ASIA-ADIGE";
+DATA_VERSION = 1;
+API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
+//SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
+VERSION = "1.0.0";
+APP_NAME="100% Riciclo - Valle dell'Adige";
+APP_NAME_CREDITS = "100% Riciclo<br/>Valle dell'Adige";
+APP_LOCATION_CREDITS = "Aldeno - Cimone - Garniga";
+ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple'};
+ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 'CRM': 'riciclo-crm', 'CRZ': 'riciclo-crz', 'Farmacia': 'riciclo-farmacia', 'Rivenditore': 'riciclo-rivenditore', 'Porta a porta carta':'riciclo-porta_a_porta', 'Porta a porta organico':'riciclo-porta_a_porta', 'Porta a porta residuo':'riciclo-porta_a_porta', 'Porta a porta cartone':'riciclo-porta_a_porta', 'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 'Porta a porta carta e cartone':'riciclo-porta_a_porta', 'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 'Porta a porta vetro':'riciclo-porta_a_porta', 'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 'Ditta specializzata':'riciclo-furgone', 'Esercenti convenzionati':'riciclo-farmacia'};
+SHOW_NEWS = true;
+FEED_URL = "http://www.asia.tn.it/rss/feed/avvisi/";
+EXT_URL = "http://www.asia.tn.it/";
+IF_HIDDEN_FIELDS = false;
+CUSTOM_PAP = [];
+ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
+PORTA_A_PORTA_LABEL = 'porta_a_porta';
+
+USE_DRAFT = true;
+LANG = ['it'];
+COMUNI_LIST = [];
+
+ZOOM = 11,5;
+CENTER = {
+      latitude: 46.1555397,
+      longitude: 11.1601932
+};
