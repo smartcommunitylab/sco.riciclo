011ba0b0934f0c06d76126bd70f2dd24068ec08b
emmartu <lelemartufi@gmail.com>
2016-02-09 16:24:11 +0100

    config ASVO

---
+++ mobile/config/instances/ASVO/www/js/props.js
@@ -0,0 +1,27 @@
+APP_ID = "ASVO";
+DATA_VERSION = 1;
+API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
+//SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
+VERSION = "1.0.0";
+APP_NAME="100% Riciclo - ASVO s.p.a";
+APP_NAME_CREDITS = "100% Riciclo<br/>ASVO s.p.a";
+APP_LOCATION_CREDITS = "ASVO";
+ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
+ICON_POINT_MAP = {};
+SHOW_NEWS = false;
+FEED_URL = "http://www.asia.tn.it/rss/feed/avvisi/";
+EXT_URL = "http://www.asia.tn.it/";
+IF_HIDDEN_FIELDS = false;
+CUSTOM_PAP = [];
+ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
+PORTA_A_PORTA_LABEL = 'porta_a_porta';
+
+USE_DRAFT = true;
+LANG = ['it'];
+COMUNI_LIST = [];
+
+ZOOM = 11;
+CENTER = {
+      latitude: 45.428414,
+      longitude: 10.986866
+};
