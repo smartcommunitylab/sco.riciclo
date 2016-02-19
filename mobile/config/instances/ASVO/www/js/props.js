6d4acb3cd7a2b7ddc6cbcb7e6a672a898f684f1d
emmartu <lelemartufi@gmail.com>
2016-02-19 13:41:49 +0100

    new data version for ASVO

--- mobile/config/instances/ASVO/www/js/props.js
+++ mobile/config/instances/ASVO/www/js/props.js
@@ -1,5 +1,5 @@
 APP_ID = "ASVO";
-DATA_VERSION = 7;
+DATA_VERSION = 13;
 API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
 //SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
 VERSION = "0.1.0";
