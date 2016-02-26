13323b431305b31e01abdacb123fc47800eb8053
micnori <micnori@yahoo.com>
2016-02-26 10:53:52 +0100

    asia rot: nuovi colori

--- mobile/config/instances/ASIA-ROT/www/js/props.js
+++ mobile/config/instances/ASIA-ROT/www/js/props.js
@@ -6,8 +6,39 @@ VERSION = "1.0.2";
 APP_NAME="100% Riciclo - Rotaliana";
 APP_NAME_CREDITS = "100% Riciclo<br/>Rotaliana";
 APP_LOCATION_CREDITS = "Comunità Rotaliana";
-ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple'};
-ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 'CRM': 'riciclo-crm', 'CRZ': 'riciclo-crz', 'Farmacia': 'riciclo-farmacia', 'Rivenditore': 'riciclo-rivenditore', 'Porta a porta carta':'riciclo-porta_a_porta', 'Porta a porta organico':'riciclo-porta_a_porta', 'Porta a porta residuo':'riciclo-porta_a_porta', 'Porta a porta cartone':'riciclo-porta_a_porta', 'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 'Porta a porta carta e cartone':'riciclo-porta_a_porta', 'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 'Porta a porta vetro':'riciclo-porta_a_porta', 'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 'Ditta specializzata':'riciclo-furgone', 'Esercenti convenzionati':'riciclo-farmacia'};
+ICON_COLOR_MAP = {'ARANCIONE': '#e69500', 
+'AZZURRO': '#00FFFF', 
+'BLU': '#0000FF', 
+'GIALLO': '#FFFF00', 
+'MARRONE': '#800000', 
+'ROSSO': '#FF0000', 
+'VERDE CHIARO': '#008000', 
+'VERDE': '#008000', 
+'VERDE SCURO': '#808000', 
+'VIOLA':'#800080',
+'GRIGIO':'#808080'};
+ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica',
+'Campane indumenti': 'riciclo-isola_ecologica', 
+'CRM': 'riciclo-crm', 
+'Cam': 'riciclo-furgone', 
+'Porta a porta residuo':'riciclo-porta_a_porta', 
+'Porta a porta organico':'riciclo-porta_a_porta', 
+'Porta a porta cartone':'riciclo-porta_a_porta', 
+'Porta a porta carta':'riciclo-porta_a_porta', 
+'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 
+'Porta a porta carta e cartone':'riciclo-porta_a_porta', 
+'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 
+'Porta a porta vetro':'riciclo-porta_a_porta', 
+'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 
+'Servizio a domicilio sfalci e potature': 'riciclo-furgone',
+'Servizio a domicilio ingombranti': 'riciclo-furgone',
+'Servizio conto terzi': 'riciclo-furgone',
+'Farmacia': 'riciclo-farmacia', 
+'Rivenditore': 'riciclo-rivenditore', 
+'Ditta specializzata':'riciclo-furgone', 
+'Esercenti convenzionati medicinali':'riciclo-farmacia',
+'Esercenti convenzionati pile': 'riciclo-rivenditore',
+'Esercenti convenzionati': 'riciclo-rivenditore'};
 SHOW_NEWS = true;
 FEED_URL = "http://www.asia.tn.it/rss/feed/avvisi/";
 EXT_URL = "http://www.asia.tn.it/";
