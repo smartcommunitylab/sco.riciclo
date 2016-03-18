978c67fdea8b3238db26b9eebd790c2a836b67d4
micnori <micnori@yahoo.com>
2016-03-18 15:28:33 +0100

    ESACOM: nuove icone

--- mobile/config/instances/ESACOM/www/js/props.js
+++ mobile/config/instances/ESACOM/www/js/props.js
@@ -7,10 +7,23 @@ APP_NAME="100% Riciclo";
 APP_NAME_CREDITS = "100% Riciclo";
 APP_LOCATION_CREDITS = "ESACOM";
 ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
-ICON_POINT_MAP = {};
+ICON_POINT_MAP = {'Ecobox':'riciclo-esacom-ecobox', 
+'Isola ecologica' : 'riciclo-esacom-isola_ecologica',
+'Punti di raccolta rup' : 'riciclo-esacom-punto_raccolta_rup',
+'Esa-com n. verde' : 'riciclo-esacom-n_verde',
+'Porta a porta carta e cartone' : 'riciclo-esacom-pp_cartone',
+'Porta a porta plastica e lattine' : 'riciclo-esacom-porta_porta',
+'Porta a porta vetro' : 'riciclo-esacom-pp_vetro',
+'Porta a porta secco' : 'riciclo-esacom-porta_porta',
+'Porta a porta verde ramaglie' : 'riciclo-esacom-erba_ramaglie',
+'Porta a porta ingombranti gratuito' : 'riciclo-esacom-pp_ingombranti_gratuito',
+'Porta a porta ingombranti a pagamento' : 'riciclo-esacom-pp_ingombranti_pagamento',
+'Porta a porta umido' : 'riciclo-esacom-pp_umido',
+'Porta a porta pannolini' : 'riciclo-esacom-porta_porta',
+'Contenitori humana/caritas' : 'riciclo-esacon-humana_caritas'};
 SHOW_NEWS = false;
-FEED_URL = "http://www.asia.tn.it/rss/feed/avvisi/";
-EXT_URL = "http://www.asia.tn.it/";
+FEED_URL = "";
+EXT_URL = "";
 IF_HIDDEN_FIELDS = false;
 CUSTOM_PAP = [];
 ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
