fe19008201d85974e1e2ca27c2e44a89f2692010
micnori <micnori@yahoo.com>
2016-03-21 14:21:08 +0100

    dati TRENTO

--- mobile/config/instances/TRENTO/www/js/props.js
+++ mobile/config/instances/TRENTO/www/js/props.js
@@ -1,32 +1,46 @@
-// TRENTO
 APP_ID = "TRENTO";
-DATA_VERSION = 11;
+DATA_VERSION = 1;
 API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
 //SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
 VERSION = "1.0.3";
-APP_NAME="100% Riciclo - Rovereto";
+APP_NAME="100% Riciclo - Trento";
 APP_NAME_CREDITS = "100% Riciclo<br/>TRENTO";
 APP_LOCATION_CREDITS = "a Trento";
 ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
-ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 'CRM': 'riciclo-crm', 'CRZ': 'riciclo-crz', 'Farmacia': 'riciclo-farmacia', 'Rivenditore': 'riciclo-rivenditore', 'Porta a porta carta':'riciclo-porta_a_porta', 'Porta a porta organico':'riciclo-porta_a_porta', 'Porta a porta residuo':'riciclo-porta_a_porta', 'Porta a porta cartone':'riciclo-porta_a_porta', 'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 'Porta a porta carta e cartone':'riciclo-porta_a_porta', 'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 'Porta a porta vetro':'riciclo-porta_a_porta', 'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 'Ditta specializzata':'riciclo-furgone', 'Esercenti convenzionati':'riciclo-farmacia', 'tipoPuntoRaccoltaUno':'riciclo-farmacia'};
+ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica', 
+'CRM': 'riciclo-crm', 
+'CRZ': 'riciclo-crz', 
+'Farmacia': 'riciclo-farmacia', 
+'Rivenditore': 'riciclo-rivenditore', 
+'Porta a porta carta':'riciclo-porta_a_porta', 
+'Porta a porta organico':'riciclo-porta_a_porta', 
+'Porta a porta residuo':'riciclo-porta_a_porta', 
+'Porta a porta cartone':'riciclo-porta_a_porta', 
+'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 
+'Porta a porta carta e cartone':'riciclo-porta_a_porta', 
+'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 
+'Porta a porta vetro':'riciclo-porta_a_porta', 
+'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 
+'Ditta specializzata':'riciclo-furgone', 
+'Esercenti convenzionati':'riciclo-farmacia',
+'Servizi a domanda individuale':'riciclo-furgone'};
 SHOW_NEWS = false;
-FEED_URL = "http://www.asvo.it/feed/";
-EXT_URL = "http://www.asvo.it";
+FEED_URL = "";
+EXT_URL = "";
 IF_HIDDEN_FIELDS = false;
 CUSTOM_PAP = [];
 //ENDPOINT_URL = 'https://tn.smartcommunitylab.it/riciclo';
-//ENDPOINT_URL = 'http://localhost:8001/riciclo';
+//ENDPOINT_URL = 'http://localhost:8080/riciclo';
 ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
 PORTA_A_PORTA_LABEL = 'porta_a_porta';
 
 USE_DRAFT = true;
-LANG = ['it','en'];
-//COMUNI_LIST = ['022205','022003','022034'];
-COMUNI_LIST = ['022205','022003','022034'];
+LANG = ['it'];
+COMUNI_LIST = ['022205'];
 
 
-ZOOM = 12;
+ZOOM = 11;
 CENTER = {
-      latitude: 45.890612,
-      longitude: 11.039560
+      latitude: 46.067289,
+      longitude: 11.121435
 };
