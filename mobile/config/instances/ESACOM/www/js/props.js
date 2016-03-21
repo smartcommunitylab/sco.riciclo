97eced8846970c4011f4d2beb145c47bd3766438
emmartu <lelemartufi@gmail.com>
2016-03-21 11:25:33 +0100

    new data and props aligned for ESACOM

--- mobile/config/instances/ESACOM/www/js/props.js
+++ mobile/config/instances/ESACOM/www/js/props.js
@@ -1,22 +1,22 @@
 APP_ID = "ESACOM";
-DATA_VERSION = 1;
+DATA_VERSION = 2;
 API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
 //SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
-VERSION = "1.0.0";
-APP_NAME="100% Riciclo";
-APP_NAME_CREDITS = "100% Riciclo";
+VERSION = "0.0.1";
+APP_NAME="100% Riciclo - ESACOM";
+APP_NAME_CREDITS = "100% Riciclo<br/>ESACOM";
 APP_LOCATION_CREDITS = "ESACOM";
 ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
 ICON_POINT_MAP = {};
 SHOW_NEWS = false;
 FEED_URL = "http://www.asia.tn.it/rss/feed/avvisi/";
-EXT_URL = "http://www.asia.tn.it/";
+EXT_URL = "http://www.esacom.it/";
 IF_HIDDEN_FIELDS = false;
 CUSTOM_PAP = [];
-ENDPOINT_URL = 'https://dev.smartcommunitylab.it/riciclo2';
+ENDPOINT_URL = 'https://tn.smartcommunitylab.it/riciclo2';
 PORTA_A_PORTA_LABEL = 'porta_a_porta';
 
-USE_DRAFT = true;
+USE_DRAFT = false;
 LANG = ['it'];
 COMUNI_LIST = [];
 
