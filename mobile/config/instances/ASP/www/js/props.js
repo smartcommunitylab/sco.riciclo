05f9e62ac09e21cccb81cdd0558306fe3a661e54
emmartu <lelemartufi@gmail.com>
2016-10-25 16:46:02 +0200

    New Config for ASP

---
+++ mobile/config/instances/ASP/www/js/props.js
@@ -0,0 +1,51 @@
+APP_ID = "ASP";
+DATA_VERSION = 34;
+API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
+//SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
+VERSION = "1.0.0";
+APP_NAME="100% Riciclo - ASP S.p.A.";
+APP_NAME_CREDITS = "100% Riciclo<br/>ASP S.p.A.";
+APP_LOCATION_CREDITS = "ASVO";
+ICON_COLOR_MAP = {'ARANCIONE': 'orange', 'AZZURRO': 'lightblue', 'BLU': 'blue', 'GIALLO': 'yellow', 'MARRONE': 'brown', 'ROSSO': 'red', 'VERDE CHIARO': 'lightgreen', 'VERDE': 'green', 'VERDE SCURO': 'darkgreen', 'VIOLA':'purple','coloreUno':'purple'};
+ICON_POINT_MAP = {'Acquisto sacchi':'riciclo-asvo-acquisto_sacchi',
+'Campana caritas':'riciclo-asvo-caritas_humana',
+'Centro ecologico mobile':'riciclo-asvo-centro_ecologico_mobile',
+'Distribuzione sacchi':'riciclo-asvo-distribuzione_sacchi',
+'Ecocentro':'riciclo-asvo-ecocentro',
+'Erba e ramaglie (punto raccolta territorio)':'riciclo-asvo-erba_ramaglie',
+'Erba e ramaglie (su prenotazione)':'riciclo-asvo-erba_ramaglie_prenotazione',
+'Impianto asvo':'riciclo-asvo-impianto',
+'Ingombranti (su prenotazione)':'riciclo-asvo-ingombranti_prenotazione',
+'Numero verde':'riciclo-asvo-n_verde-59',
+'Porta a porta carta e cartone':'riciclo-asvo-pp_carta_cartone',
+'Porta a porta erba e ramaglie':'riciclo-asvo-erba_ramaglie',
+'Porta a porta erba e ramaglie (a pagamento)':'riciclo-asvo-erba_ramaglie',
+'Porta a porta imballaggi in plastica, lattine':'riciclo-asvo-porta_porta',
+'Porta a porta pannolini':'riciclo-asvo-pp_pannolini',
+'Porta a porta secco non riciclabile':'riciclo-asvo-porta_porta',
+'Porta a porta umido':'riciclo-asvo-bidone',
+'Porta a porta vetro, imballaggi in plastica, lattine':'riciclo-asvo-bidone',
+'Punti raccolta sul territorio':'riciclo-asvo-punto_raccolta_territorio',
+'Raccolta pericolosi':'riciclo-asvo-raccolta_pericolosi',
+'Stradale carta e cartone':'riciclo-asvo-bidone',
+'Stradale secco non riciclabile':'riciclo-asvo-bidone',
+'Stradale umido':'riciclo-asvo-bidone',
+'Stradale vetro':'riciclo-asvo-bidone',
+'Stradale vetro, plastica, lattine':'riciclo-asvo-bidone'};
+SHOW_NEWS = true;
+FEED_URL = "";
+EXT_URL = "http://www.asp.asti.it/";
+IF_HIDDEN_FIELDS = false;
+CUSTOM_PAP = [];
+ENDPOINT_URL = 'https://tn.smartcommunitylab.it/riciclo2';
+PORTA_A_PORTA_LABEL = 'porta_a_porta';
+
+USE_DRAFT = false;
+LANG = ['it'];
+COMUNI_LIST = [];
+
+ZOOM = 10;
+CENTER = {
+      latitude: 44.898694,
+      longitude: 8.205470
+};
