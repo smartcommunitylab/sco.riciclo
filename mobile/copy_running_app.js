190e865385d1f634f6f9e26320b9ff377babf7a1
micnori <micnori@yahoo.com>
2017-10-30 09:43:07 +0100

    tolte risorse da www generale e aggiunto allo script la copia delle risorse specifiche in folder RUNNING

--- mobile/copy_running_app.js
+++ mobile/copy_running_app.js
@@ -4,7 +4,7 @@ var fs = require('fs-extra');
 ncp.limit = 16;
 
 var profile = null;
-console.log('init copy_running');
+console.log('check 1');
 
 if (process.argv.length < 3) {
     console.error("Profile name arg is missing, read curren_profile.txt...");
@@ -12,34 +12,66 @@ if (process.argv.length < 3) {
     console.log("Found "+profile);
 } else {
     profile = process.argv[2];
-    console.log("Not Found "+profile);
 }
 
 if (profile == null) {
     console.error("Profile name is missing!");
 } else {
+    console.log('check 2');
 
-    fs.copy("../mobile", "../../RUNNING/"+profile, function (err) {
-        console.log('fs.copy -- from: ../mobile - to: ../../RUNNING/'+profile);
+    fs.copy("../mobile", "../../riciclo_running/"+profile, function (err) {
+        console.log('check 3');
 
         if (err) {
           console.error("copy app err: "+err);
         } else {
             console.log('copy app done!');
 
-            ncp("config/instances/"+profile+"/www", "../../RUNNING/"+profile+"/www", function (err) {
-                 console.log('fs.copy -- from: config/instances/'+profile+'/www - to: ../../RUNNING/'+profile+'/www');
+            ncp("config/instances/"+profile+"/www", "../../riciclo_running/"+profile+"/www", function (err) {
+                 console.log('check 3.1');
 
                  if (err) {
                    console.error("copy config err: "+err);
                  }
 
-                console.log('copy config done!');
+                console.log('copy config www done!');
+            });
+						
+            ncp("config/instances/"+profile+"/resources", "../../riciclo_running/"+profile+"/resources", function (err) {
+                 console.log('check 3.2');
+
+                 if (err) {
+                   console.error("copy config err: "+err);
+                 }
+
+                console.log('copy config resources done!');
             });
 
         }
     });
 
+   //ncp("../mobile", "../../RUNNING/", function (err) {
+   //    console.log('check 3');
+
+   //    if (err) {
+   //      console.error("copy app err: "+err);
+   //    }
+   //
+   //    console.log('copy app done!');
+
+   //    ncp("config/instances/"+profile+"/www", "../../RUNNING/mobile/www", function (err) {
+   //         console.log('check 3.1');
+
+   //         if (err) {
+   //           return console.error("copy config err: "+err);
+   //         }
+
+   //         console.log('copy config done!');
+   //    });
+   //});
+
+    console.log('check 4');
+
 }
 
-console.log('end copy_running');
+console.log('check 5');
