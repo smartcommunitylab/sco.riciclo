65460418129f648a4195d73f3560a03424d25842
micnori <micnori@yahoo.com>
2018-06-05 14:23:00 +0200

    add GIUDICARIE

---
+++ mobile/config/instances/GIUDICARIE/www/js/props.js
@@ -0,0 +1,59 @@
+APP_ID = "GIUDICARIE";
+DATA_VERSION = 2;
+API_KEY = "AIzaSyBmKVWmFzh2JHT7q1MLmQRQ7jC4AhkRBDs";
+//SEGNALA_EMAIL = "segnalazioni.rifiuti@comunitadellegiudicarie.it";
+VERSION = "1.0.1";
+APP_NAME="100% Riciclo - Giudicarie";
+APP_NAME_CREDITS = "100% Riciclo<br/>Giudicarie";
+APP_LOCATION_CREDITS = "Comunità della Giudicarie";
+ICON_COLOR_MAP = {'ARANCIONE': '#e69500', 
+'AZZURRO': '#00FFFF', 
+'BLU': '#0000FF', 
+'GIALLO': '#FFFF00', 
+'MARRONE': '#800000', 
+'ROSSO': '#FF0000', 
+'VERDE CHIARO': '#008000', 
+'VERDE': '#008000', 
+'VERDE SCURO': '#808000', 
+'VIOLA':'#800080',
+'GRIGIO':'#808080'};
+ICON_POINT_MAP = {'Isola ecologica': 'riciclo-isola_ecologica',
+'Campane indumenti': 'riciclo-isola_ecologica', 
+'CRM': 'riciclo-crm', 
+'Cam': 'riciclo-furgone', 
+'Porta a porta residuo':'riciclo-porta_a_porta', 
+'Porta a porta organico':'riciclo-porta_a_porta', 
+'Porta a porta cartone':'riciclo-porta_a_porta', 
+'Porta a porta carta':'riciclo-porta_a_porta', 
+'Porta a porta plastica e metallo':'riciclo-porta_a_porta', 
+'Porta a porta carta e cartone':'riciclo-porta_a_porta', 
+'Porta a porta imballaggi leggeri':'riciclo-porta_a_porta', 
+'Porta a porta vetro':'riciclo-porta_a_porta', 
+'Porta a porta sfalci e potatura':'riciclo-porta_a_porta', 
+'Servizio a domicilio sfalci e potature': 'riciclo-furgone',
+'Servizio a domicilio ingombranti': 'riciclo-furgone',
+'Servizio conto terzi': 'riciclo-furgone',
+'Farmacia': 'riciclo-farmacia', 
+'Rivenditore': 'riciclo-rivenditore', 
+'Ditta specializzata':'riciclo-furgone', 
+'Esercenti convenzionati medicinali':'riciclo-farmacia',
+'Esercenti convenzionati pile': 'riciclo-rivenditore',
+'Esercenti convenzionati': 'riciclo-rivenditore'};
+SHOW_NEWS = false;
+FEED_URL = "http://news.it/";
+EXT_URL = "http://www.news.it/";
+IF_HIDDEN_FIELDS = false;
+CUSTOM_PAP = [];
+ENDPOINT_URL = 'https://tn.smartcommunitylab.it/riciclo2';
+PORTA_A_PORTA_LABEL = 'porta_a_porta';
+TOPONIMO_SEPARATOR = "<p>";
+
+USE_DRAFT = false;
+LANG = ['it'];
+COMUNI_LIST = [];
+
+ZOOM = 9;
+CENTER = {
+      latitude: 46.015649,
+      longitude: 10.706269
+};
