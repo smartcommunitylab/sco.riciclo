a33c1f77de8d08b38d16f219a05036eca3186502
micnori <micnori@yahoo.com>
2019-03-04 13:09:03 +0100

    fix config.xml hooks

--- mobile/build_android.bat
+++ mobile/build_android.bat
@@ -9,8 +9,11 @@ if exist ".\build\%profile%-release.apk" (
 )
 
 cd "..\..\riciclo_running\%profile%"
+set rootDir=%CD%
 echo "Android build %profile%"
 call npm i -D -E @ionic/v1-toolkit
+node .\hooks\before_prepare\005_move_proto_config_file.js "%rootDir%"
+node .\hooks\before_prepare\010_replace_text.js "%rootDir%"
 call ionic cordova platform rm android
 call ionic cordova platform add android@7.0.0
 call ionic cordova build android --release
@@ -21,8 +24,8 @@ if exist ".\build\%profile%.apk" (
 	del ".\build\%profile%.apk"
 )
 
-call jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .\build\%profile%.keystore -storepass %password% .\build\%profile%-release.apk %alias%
-call jarsigner -verify -certs -verbose .\build\%profile%-release.apk
-call zipalign -v 4 .\build\%profile%-release.apk .\build\%profile%.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .\build\%profile%.keystore -storepass %password% .\build\%profile%-release.apk %alias%
+jarsigner -verify -certs -verbose .\build\%profile%-release.apk
+zipalign -v 4 .\build\%profile%-release.apk .\build\%profile%.apk
 del .\build\%profile%-release.apk
 
